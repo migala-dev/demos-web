b56ec43cc1c0cb47e2d9bd3647dad6bb6066bf69
Alex Ortega <alexortegazj@gmail.com>
2022-06-03 12:41:08 -0700

    Creating first layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import logo from "./assets/demos-logo.png";
+import googlePlayBadge from "./assets/google-play-badge.png";
+import appStoreBadge from "./assets/app-store-badge.svg";
+import personWithPhone1 from "./assets/person-with-phone-1.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="content-container">
+        <img className="logo" src={logo} alt="logo"></img>
+        <div className="stores-badges">
+          <img className="badge" src={googlePlayBadge} alt="google-play"></img>
+          <img className="badge" src={appStoreBadge} alt="app-store"></img>
+        </div>
+        <div className="actions-container">
+          <div className="action-container">
+            <label>Conoce nuestras:</label>
+            <div className="button primary">Politicas de privacidad</div>
+          </div>
+          <div className="action-container">
+            <label>Nuestro es codigo libre, encuentralo en:</label>
+            <div className="button primary">Repositorios</div>
+          </div>
+        </div>
+        <div className="actions-container">
+          <div className="action-container">
+            <label>Alguna:</label>
+            <div className="button primary">Queja y/o sugerencias</div>
+          </div>
+          <div className="action-container">
+            <label>Encuentranos en:</label>
+            <div className="button primary">Redes sociales</div>
+          </div> 
+        </div>
+      </div>
+      <img
+        className="person-with-phone"
+        src={personWithPhone1}
+        alt="person-with-phone"
+      ></img>
     </div>
   );
 }
