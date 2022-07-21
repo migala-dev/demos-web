b4767c73c5eeac43fa0db52ebc6578ed3c9ffbd8
YaelBadillo <yael.badillos@gmail.com>
2022-07-21 04:28:09 -0500

    Feat: Add back button to return to the previous page

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,15 +1,31 @@
 import PropTypes from 'prop-types';
 
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import logo from 'assets/demos-logo.png';
 import personWithPhone1 from 'assets/person-with-phone-1.png';
 
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="app-container">
       <div className="content-container">
+        
+        {pathname !== '/' ? (
+          <button onClick={handleBack} className="back-button">
+            <span className="material-icons-round back-button__icon">arrow_back</span>
+          </button>
+        ) : null}
+
         <img className="logo" src={logo} alt="logo"></img>
 
-        { children }
+        {children}
 
       </div>
 
