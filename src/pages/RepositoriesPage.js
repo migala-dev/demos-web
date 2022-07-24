af289184a5ed9dbe2d82af2820f4c1890a93d31e
YaelBadillo <yael.badillos@gmail.com>
2022-07-24 15:54:23 -0500

    Feat: Add repository lists

--- src/pages/RepositoriesPage.js
+++ src/pages/RepositoriesPage.js
@@ -1,22 +1,28 @@
 import RepositoryList from 'components/RepositoryList';
 import RepositoryElement from 'components/RepositoryElement';
 import RepositoryCard from 'components/RepositoryCard';
+import repositories from 'utils/repositories';
 
 const RepositoriesPage = () => {
+  const lists = Object.keys(repositories).map(lista => (  
+    <RepositoryList key={lista} listTitle={lista}>
+
+      {repositories[lista].map(({ name, repositoryName }) => (
+        <RepositoryElement key={name} elementTitle={name}>
+          <RepositoryCard 
+            repositoryURL={`https://api.github.com/repos/AlexOrtegaZ/${repositoryName}`}
+          />
+        </RepositoryElement>
+      ))}
+
+    </RepositoryList>
+  ));
+
   return (
     <section className="repositories-page">
       <h1 className="repositories-page__title">Repositorios</h1>
 
-      {/* Test list */}
-      <RepositoryList listTitle="Backend">
-
-        <RepositoryElement elementTitle="Space Service">
-          <RepositoryCard 
-            repositoryURL="https://api.github.com/repos/AlexOrtegaZ/demos-space-service"
-          />
-        </RepositoryElement>
-
-      </RepositoryList> 
+      {lists} 
     </section>
   );
 };
---
+++ src/utils/repositories.js
@@ -0,0 +1,19 @@
+const repositories = {
+  app: [
+    { name: 'App', repositoryName: 'demos-app' }
+  ],
+
+  backend: [
+    { name: 'Auth Service', repositoryName: 'demos-auth-service' },
+    { name: 'Space Service', repositoryName: 'demos-space-service' },
+    { name: 'Cache Service', repositoryName: 'demos-cache-service' },
+    { name: 'Manifesto Service', repositoryName: 'demos-manifesto-service' },
+    { name: 'Websocket', repositoryName: 'demos-websocket' }
+  ],
+
+  submodule: [
+    { name: 'demos-shared', repositoryName: 'demos-shared' }
+  ]
+};
+
+export default repositories;
